29c159a125dbecbe77493db5171b1db9deffb832
unluckycord <loganknapp2@gmail.com>
2023-05-31 14:53:13 -0700

    Uploading changes

--- GUI.java
+++ GUI.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -7,22 +10,31 @@ public class GUI {
         int WINDOWHEIGHT = 500;
         int WINDOWWITDH = 600;
 
+        int width = 20;
+        int height = 40;
 
-        JFrame frame = new JFrame("Java Search Engine", null);
+        for(int i =0; i< 600; i++){
+            width++;
+            height++;
+        }
+
+        
+
+        JFrame frame = new JFrame("Java Search Engine");
         JPanel panel = new JPanel();
 
 
         frame.setSize(WINDOWWITDH, WINDOWHEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
 
-        JTextField SearchBox = new JTextField(10);
+        JTextField SearchBox = new JTextField(50);
         //SearchBox.setBounds(400, 500, 10, 40);
         //SearchBox.setSize(100, 40);
-        SearchBox.setLocation(100, 200);
-        panel.add(SearchBox);
+        SearchBox.setPreferredSize(new Dimension(50, 40));
 
         //This must be at the end, if not, the program will not render correctly
+        panel.add(SearchBox, BorderLayout.SOUTH);
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
